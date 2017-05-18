bc7b29a9ec0eb782ae62941a4217e4e25d8066cd
root <root@localhost.localdomain>
2017-05-18 14:58:59 -0400

    First commit

---
+++ bot.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const Wit = require('node-wit').Wit;
+const FB = require('./facebook.js');
+const Config = require('./const.js');
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+// Bot actions
+const actions = {
+  say(sessionId, context, message, cb) {
+    console.log(message);
+
+    // Bot testing mode, run cb() and return
+    if (require.main === module) {
+      cb();
+      return;
+    }
+
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to from context
+    // TODO: need to get Facebook user name
+    const recipientId = context._fbid_;
+    if (recipientId) {
+      // Found our recipient!
+      FB.fbMessage(recipientId, message, (err, data) => {
+        if (err) {
+          console.log(
+            'Oops! An error occurred while forwarding the response to',
+            recipientId,
+            ':',
+            err
+          );
+        }
+
+        // Let's give the wheel back to our bot
+        cb();
+      });
+    } else {
+      console.log('Oops! Couldn\'t find user in context:', context);
+      // Giving the wheel back to our bot
+      cb();
+    }
+  },
+    merge(sessionId, context, entities, message, cb) {
+    // Retrieve the entity and store it into a context field
+    const eve = firstEntityValue(entities, 'tathva_event');
+    if (eve) {
+      context.eve = eve; // store it in context
+    }
+
+    cb(context);
+  },
+
+  error(sessionId, context, error) {
+    console.log(error.message);
+  },
+};
+
+
+const getWit = () => {
+  return new Wit(Config.WIT_TOKEN, actions);
+};
+
+exports.getWit = getWit;
+
+
+if (require.main === module) {
+  console.log("Bot testing mode.");
+  const client = getWit();
+  client.interactive();
+}
+
